31105c6960ac03f3df4cf6a9a399d9614c669657
menyin <845257580@qq.com>
2018-01-26 18:13:11 +0800

    增加web层相关目录

---
+++ src/com/cny/core/dao/impl/BaseDaoImpl.java
@@ -0,0 +1,43 @@
+package com.cny.core.dao.impl;
+
+import com.cny.core.dao.BaseDao;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
+
+public abstract class BaseDaoImpl<T> extends HibernateDaoSupport implements BaseDao<T> {
+    private Class<T> clazz;
+
+    public BaseDaoImpl() {
+        ParameterizedType pt =  (ParameterizedType)this.getClass().getGenericSuperclass();//BaseDaoImpl<User>
+        clazz = (Class<T>)pt.getActualTypeArguments()[0];
+    }
+
+    @Override
+    public void save(T entity) {
+
+        getHibernateTemplate().save(entity);
+    }
+
+    @Override
+    public void update(T entity) {
+        getHibernateTemplate().update(entity);
+    }
+
+    @Override
+    public void delete(Serializable id) {
+        getHibernateTemplate().delete(findObjectById(id));
+    }
+
+    @Override
+    public T findObjectById(Serializable id) {
+        return getHibernateTemplate().get(clazz, id);
+    }
+
+    @Override
+    public List<T> findObjects() {
+        return null;
+    }
+}
